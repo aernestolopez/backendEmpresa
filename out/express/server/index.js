fae1fa7bed8741937ffbf4cfdbc5fc613c61ff68
sokas <aernestolh@gmail.com>
2023-03-10 09:39:59 +0100

    Protocolos Seguros

--- out/express/server/index.js
+++ out/express/server/index.js
@@ -9,21 +9,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 const express_1 = __importDefault(require("express"));
 const route_1 = __importDefault(require("../routes/route"));
-const https_1 = __importDefault(require("https"));
-const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 app.use(express_1.default.urlencoded({ extended: true, limit: '100000kb' }));
 app.use(express_1.default.json({ limit: '100000kb' }));
 app.use(route_1.default);
 const PORT = 1289;
 exports.default = app;
-/*app.listen(PORT, ()=>
-    console.log(`Corriendo en ${PORT}`));*/
-https_1.default
-    .createServer({
-    key: fs_1.default.readFileSync("src/cert/key.pem"),
-    cert: fs_1.default.readFileSync("src/cert/cert.pem"),
-}, app)
-    .listen(PORT, () => {
-    console.log("serever is runing at port 4000");
-});
+app.listen(PORT, () => console.log(`Corriendo en ${PORT}`));
+/*
+Creacion de Protocolos seguros. Comentado ya que flutter no acepta
+autofirmados por lo que la aplicacion frontend no funciona aunque si
+funcione las llamadas con Postman
+
+https
+.createServer(
+  {
+    key: fs.readFileSync("src/cert/key.pem"),
+    cert: fs.readFileSync("src/cert/cert.pem"),
+  },
+  app
+)
+.listen(PORT, () => {
+  console.log(`Corriendo en ${PORT}`);
+});*/
